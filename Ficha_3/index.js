d4fabd9213d617297e24cdf5f31e99b31420c073
AFGRocha <antoniolaranjinha@hotmail.com>
2018-03-16 12:45:00 +0000

    Ficha 3 ate ao H (Nota: falta o ex g)

--- Ficha_3/index.js
+++ Ficha_3/index.js
@@ -72,5 +72,125 @@ function funcD(){
 //ex E
 
 function funcE(){
+
+    let word = prompt("Indique uma palavra:")
+    let reverse = ""
+
     
+    //Percorre a string e inverte
+    for(let i = word.length - 1; i >= 0; i--){
+
+        reverse = reverse + word[i]
+    }
+
+    show("E - > "+ reverse)
+
 }
+
+//Ex F
+
+function funcF(){
+
+    let word = prompt("Indique uma palavra:")
+    let chara = prompt("Indique uma letra dessa palavra")
+    let newWord = ""
+
+    //Se a letra estiver na palavra
+    if (word.indexOf(chara) != -1){
+        //Percorre a string 
+        for(let i = 0; i < word.length; i++){
+
+
+            //Quando encontra a letra na string não copia
+            if(word[i] == chara){
+    
+            }
+             
+            //Copia as letras para o novo string
+            else {
+            newWord = newWord + word[i] 
+            }
+            
+        }
+
+        show("F - > "+ newWord)
+    }
+
+    else{
+        show("F - > essa letra não se encontra não se encontra na palavra")
+    }
+
+}
+
+//Outra forma para o F
+function funcF2(){
+
+    let word = prompt("Indique uma palavra:")
+    let chara = prompt("Indique uma letra dessa palavra")
+    let finder = word.indexOf("chara")
+
+    if (word.indexOf(chara) != -1){
+
+        word = word.substring(0,finder) + word.substring(finder+1, word.length-1)
+
+        show("F - > "+ word)
+    }
+
+    else{
+        show("F - > essa letra não se encontra não se encontra na palavra")
+    }
+}
+
+
+//Ex G
+
+function funcG(){
+
+}
+
+
+//Ex H
+function funcH(){
+
+
+    let min =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","u","v","w","x","y","z"];
+
+    let mai =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","U","V","W","X","Y","Z"];
+
+
+
+    let word = prompt("Indique a palavra a encriptar:")
+    let newWord = "";
+    let finderMin
+    let finderMai
+
+    for(let i = 0; i < word.length;i++){
+
+        finderMin = min.indexOf(word[i])
+        finderMai = mai.indexOf(word[i])
+
+        if( finderMin != -1){
+
+            newWord = newWord + min[finderMin+3]
+        }
+
+        else if( finderMin == -1){
+            continue
+        }
+
+        if( finderMai != -1){
+
+            newWord = newWord + min[finderMai+3]
+        }
+
+        else if( finderMai == -1){
+            continue
+        }
+        
+    }
+    
+    show("H - > "+ newWord)
+}
+
+
+
