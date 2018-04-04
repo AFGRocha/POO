01130ec42570a0ca13f354d2aaf0f53cd1705642
AFGRocha <antoniolaranjinha@hotmail.com>
2018-04-04 17:34:19 +0100

    Ex2 (more or less finished)

---
+++ Ficha_7/ex02.js
@@ -0,0 +1,38 @@
+window.onload = function() {
+    init()   
+}
+
+
+
+
+function init(){
+
+    
+
+    let myBtn = document.getElementById("submit")
+    let autoFill = document.getElementsByClassName("auto") //unused
+    let name = document.getElementById("nome")
+    let number = document.getElementById("telemovel")
+    let email = document.getElementById("email")
+    let user = document.getElementById("user")
+    let password = document.getElementById("password")
+    let password2 = document.getElementById("password2")
+
+    let form = document.getElementById("myForm")
+    
+
+    
+
+    if(password.value == password2.value){
+        form.addEventListener("submit", function(){
+
+
+
+            window.alert("REGISTO EFETUADO COM SUCESSO")
+         
+    
+        
+        })
+
+    }
+}
