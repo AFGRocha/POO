b4c8340e08c2afa489bc8eee9a059f5a51176176
AFGRocha <antoniolaranjinha@hotmail.com>
2018-04-05 22:59:27 +0100

    Complete with more options

--- Ficha_7/ex02.js
+++ Ficha_7/ex02.js
@@ -1,5 +1,43 @@
 window.onload = function() {
-    init()   
+   
+    //Esta opçao permite alterar o maximo de escolha no html, abaixo tem outra feita para limitar por javascript
+
+    
+   let data = document.getElementById("bday")
+
+   //buscar o dia de hoje completo
+   let fullDate = new Date()
+   //Retirar apenas o dia e converter para string
+   let day = fullDate.getDate() - 1
+   let dayTable = day.toString()
+    //If porque JS devolve "1" e html precisa de 01
+   if(dayTable.length == 1){
+       dayTable = 0 + dayTable
+   }
+   
+   //Retirar apenas o mes e converter para string
+   let month = fullDate.getMonth() + 1 // Porque janeiro começa em 0
+   let monthTable = month.toString()
+
+   //Este if repete a mesma logica, pode ser feito uma funçao
+   if(monthTable.length == 1){
+    monthTable = 0 + monthTable
+   }
+
+
+   //Devolve o ano 
+   let year = fullDate.getFullYear()
+   // formata a data inteira de forma a que o html consiga ler
+   let today = year + "-" + monthTable + "-" + dayTable
+
+   
+   //console.log(today)
+   
+   
+   data.max = today
+   
+
+   init()   
 }
 
 
@@ -17,23 +55,35 @@ function init(){
     let user = document.getElementById("user")
     let password = document.getElementById("password")
     let password2 = document.getElementById("password2")
-
+    let bday = document.getElementById("bday")
     let form = document.getElementById("myForm")
     
 
-    
-
-    if(password.value == password2.value){
+        //Forma de comparar datas apenar por javascript
+        // Verificar se a data selecionada não é superior à data atual
+        // Date.parse() recebe uma data e converte para milissegundos (desde 1 de janeiro de 1970 até a essa data)
+        /*        
+        let dataNasc = Date.parse(bday.value)
+        let dataAtual = Date.parse(Date())
+        Resto da logica esta no if */
+        
         form.addEventListener("submit", function(event){
-
 
             event.preventDefault()
 
-            window.alert("REGISTO EFETUADO COM SUCESSO")
+            if(password.value == password2.value /* && dataNasc < dataAtual */)    { 
+                
+        
+                    window.alert("REGISTO EFETUADO COM SUCESSO")
+            }
+            else{
+                window.alert("Verifique a pass e a data")
+            }
+            
          
     
         
-        })
+        })   
 
-    }
+   
 }
