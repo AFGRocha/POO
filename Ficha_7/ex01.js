020c511f2e18d21d4d9272c2dc09431e76b9fa2a
AFGRocha <antoniolaranjinha@hotmail.com>
2018-04-05 14:47:28 +0100

    Ex 1 Redone

--- Ficha_7/ex01.js
+++ Ficha_7/ex01.js
@@ -10,7 +10,7 @@ function init(){
 
    
     //Vai buscar os elementos html
-    let myBtn = document.getElementById("submit")
+    let myForm = document.getElementById("submit")
     let myBtn2 = document.getElementById("clean")
     let myEmail = document.getElementById("email")
     let myPassword = document.getElementById("password")
@@ -18,40 +18,47 @@ function init(){
  
 
     //ao clicar no butao submeter
-    myBtn.addEventListener("click", function(){
+    myForm.addEventListener("submit", function(event){
 
-        //Console test
-        console.log(myPassword.value.length)
-        console.log(myEmail.value.indexOf(".com"))
+
+        event.preventDefault()
         
         //Entra no if se o email foi valido
-        if (myEmail.value.indexOf(".com") != -1){
+        //if (myEmail.value.indexOf(".com") != -1){
 
             //Se a passa for valida tambem, ativa o alerta
-            if (myPassword.value.length >= 7 && myPassword.value.length <= 10 ){
+           // if (myPassword.value.length >= 7 && myPassword.value.length <= 10 ){
             
-                window.alert("PASS#EMAIL");
-            }
+                window.alert(myPassword.value + "#" + myEmail.value);
+            //}
 
-            //Se nao estiver valida
+            /*Se nao estiver valida
             else{
                 window.alert("Invalid email or password");
-            }
-        }
+            } */
+       // }
 //Se nao estiver valido
-        else{
-            window.alert("Invalid email or password");
-        }
+       // else{
+         //   window.alert("Invalid email or password");
+       // }
 
     })
 
 
 
+    myForm.addEventListener("reset", function(){
+
+        myEmail.focus()
+    })
+
+
+//Nao preciso porque o html tem semantica para limpar o form
     //Clean button
+    /*
     myBtn2.addEventListener("click", function(){
 
         myEmail.value = ""
         myPassword.value = ""
        
-    })
+    })*/
 }
